e75927bf47c6a3c69c38109551bef83f308e697e
James Finkelstein <jfinkelstein9@me.com>
2018-10-24 15:17:46 -0400

    added random coloring to balls

--- scripts.js
+++ scripts.js
@@ -35,6 +35,13 @@ canvas.height = innerHeight;
 // context.arc(x,y,radius,startAngle (radians 0 just means 3 o'clock position),endAngle (radians 2 * Math.PI is 360 degrees),anticlockwise (boolean)
 // );
 
+
+//mouse object
+const mouse = {
+    x: undefined,
+    y: undefined
+}
+
 //cirlce object
 function circle (x = 200, y = 200, radius = 30, dx = Math.random() * 4, dy = Math.random() * 4) {
     this.x = x;
@@ -42,11 +49,12 @@ function circle (x = 200, y = 200, radius = 30, dx = Math.random() * 4, dy = Mat
     this.radius = radius;
     this.dx = dx;
     this.dy = dy;
+    this.color = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`
 };
 
 circle.prototype.draw = function () {
     context.beginPath();
-    context.strokeStyle = 'red';
+    context.fillStyle = this.color;
     context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
     context.fill();
 };
@@ -66,6 +74,12 @@ circle.prototype.update = function () {
     this.x += this.dx;
     this.y += this.dy;
 
+    if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50 && this.radius < 40) {
+        this.radius += 1;
+    } else if (this.radius > 10) {
+        this.radius -= 1;
+    }
+
     //draws the circle
     this.draw();
 }
@@ -74,7 +88,7 @@ circle.prototype.update = function () {
 const circleArray = [];
 
 //push new instances of circles to array
-for (i = 0; i < 100; i++) {
+for (i = 0; i < 200; i++) {
     circleArray.push(new circle(Math.floor(Math.random() * (innerWidth - 30 * 2)) + 30, Math.floor(Math.random() * (innerHeight - 30 * 2)) + 30));
 }
 
@@ -89,5 +103,17 @@ function animate() {
 
 //start animation!!!
 animate();
+
+
+
+
+
+window.addEventListener('mousemove', (e) => {
+    console.log(mouse);
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+
+
 
 
