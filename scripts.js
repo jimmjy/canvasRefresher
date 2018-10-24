39ad05a20bb754f6df2a392ed0f9a44d83b4dea5
James Finkelstein <jfinkelstein9@me.com>
2018-10-24 10:45:14 -0400

    created circle bounces

--- scripts.js
+++ scripts.js
@@ -3,9 +3,10 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
 //width height adjustments
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
+//making rectangles
 // context.fillStyle = "rgba(255, 0, 0, .5)";
 // context.fillRect(100, 100, 100, 100);
 // context.fillStyle = "rgba(0, 255, 0, .5)"
@@ -13,15 +14,15 @@ canvas.height = window.innerHeight;
 // context.fillStyle = "rgba(0, 0, 255, .5)"
 // context.fillRect(300, 300, 100, 100);
 
-// // canvas objects:
-// // 1. Rectangles
-// // 2. Lines
-// // 3. Arcs/Circles
-// // 4. Bezier Curves
-// // 5. Images
-// // 6. Text
+// canvas objects:
+// 1. Rectangles
+// 2. Lines
+// 3. Arcs/Circles
+// 4. Bezier Curves
+// 5. Images
+// 6. Text
 
-// // Lines
+// Lines
 
 // context.beginPath();
 // context.strokeStyle = '#fa34a3';
@@ -30,54 +31,12 @@ canvas.height = window.innerHeight;
 // context.lineTo(400, 300);
 // context.stroke();
 
-// //arc / circle
-// // context.arc(x,y,radius,startAngle (radians 0 just means 3 o'clock position),endAngle (radians 2 * Math.PI is 360 degrees),anticlockwise (boolean)
-// // );
+//arc / circle
+// context.arc(x,y,radius,startAngle (radians 0 just means 3 o'clock position),endAngle (radians 2 * Math.PI is 360 degrees),anticlockwise (boolean)
+// );
 
-// for (i = 0; i < 30; i++) {
-//     context.beginPath();
-//     context.arc((Math.random() * window.innerWidth) + 30, Math.random() * window.innerHeight, 30, 0, Math.PI * 2, false);
-//     context.strokeStyle = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-//     context.stroke();
-// }
-
-// context.beginPath();
-// context.strokeStyle = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-// context.arc(200, 200, 30, 0, 2*Math.PI, false);
-// context.stroke();
-
-// let x = 30;
-// let y = 30;
-// let dy = 5;
-// let dx = 5;
-// let radius = 30;
-
-// //Math.floor(Math.random() * (max - min)) + (min)
-// // -4 / 10 Math.random() * 
-
-// //recursive function
-// function animate() {
-//     requestAnimationFrame(animate);
-//     context.beginPath();
-//     context.clearRect(0, 0, innerWidth, innerHeight); // clears the whole canvas
-//     context.strokeStyle = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-//     context.arc(x, y, radius, 0, 2 * Math.PI, false);
-//     context.stroke();
-//     x+=dx;
-//     y+=dy;
-
-//     if (x >= innerWidth - radius || x < radius) {
-//         dx = -dx;
-//     }
-
-//     if (y >= innerHeight - radius || y < radius) {
-//         dy = -dy;
-//     }
-// };
-
-// animate();
-
-function circle (x = 200, y = 200, radius = 30, dx = 3, dy = 3) {
+//cirlce object
+function circle (x = 200, y = 200, radius = 30, dx = Math.random() * 4, dy = Math.random() * 4) {
     this.x = x;
     this.y = y;
     this.radius = radius;
@@ -87,28 +46,48 @@ function circle (x = 200, y = 200, radius = 30, dx = 3, dy = 3) {
 
 circle.prototype.draw = function () {
     context.beginPath();
-    context.clearRect(0, 0, innerWidth, innerHeight);
     context.strokeStyle = 'red';
     context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-    context.stroke();
+    context.fill();
 };
 
-circle.prototype.animate = function () {    
-    requestAnimationFrame(this.animate.bind(this));
-    if (this.x < 0 + this.radius || this.x + this.radius > innerWidth) {
+circle.prototype.update = function () {
+    //make sure balls don't bounce outside of edges on left and right
+    if (this.x - this.radius < 0 || this.x + this.radius > innerWidth) {
         this.dx = -this.dx;
     }
 
-    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+    //make sure balls don't bounce out top and bottom
+    if (this.y - this.radius < 0 || this.y + this.radius > innerHeight) {
         this.dy = -this.dy;
     }
 
+    //change the x and y directions when edge is hit
     this.x += this.dx;
     this.y += this.dy;
 
+    //draws the circle
     this.draw();
+}
+
+//create array for many circles
+const circleArray = [];
+
+//push new instances of circles to array
+for (i = 0; i < 100; i++) {
+    circleArray.push(new circle(Math.floor(Math.random() * (innerWidth - 30 * 2)) + 30, Math.floor(Math.random() * (innerHeight - 30 * 2)) + 30));
+}
+
+//create repeated animate function
+function animate() {
+    requestAnimationFrame(this.animate.bind(this));
+    context.clearRect(0, 0, innerWidth, innerHeight);
+    circleArray.forEach((circle) => {
+        circle.update();
+    });
 };
 
-const newCircle = new circle ();
-newCircle.animate()
+//start animation!!!
+animate();
+
 
